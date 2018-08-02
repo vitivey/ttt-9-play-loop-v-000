43ba7b3d129608c12459ffe0280d894936e18e4c
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-02 15:27:04 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -28,10 +28,12 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
+    puts "Yes"
+    #move(board, index)
+    #display_board(board)
   else
-    turn(board)
+    puts "No"
+    #turn(board)
   end
 end
 
